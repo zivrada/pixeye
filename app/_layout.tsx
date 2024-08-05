9e89cbe56e5704dfccdc56db91993d59dbff9661
Ziv <zivrada2221@gmail.com>
2024-08-06 01:29:50 +0300

    iuhgnrgefe

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="index" />
     </Stack>
   );
